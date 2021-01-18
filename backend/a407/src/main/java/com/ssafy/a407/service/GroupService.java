a9989b55b11fa2900883de177b96dad3006e1c8a
하태민 <haruto4380@gmail.com>
2021-01-18 18:42:02 +0900

    #TM05 - 그룹 검색

--- backend/a407/src/main/java/com/ssafy/a407/service/GroupService.java
+++ backend/a407/src/main/java/com/ssafy/a407/service/GroupService.java
@@ -1,9 +1,17 @@
 package com.ssafy.a407.service;
 
+import java.util.List;
 import java.util.Map;
+
+import com.ssafy.a407.dto.GroupDto;
 
 public interface GroupService {
 
 	public int remove(int gId) throws Exception;
 	public int update(Map mem) throws Exception;
+	public List<GroupDto> searchList() throws Exception;
+	public List<GroupDto> searchGroupName(String word) throws Exception;
+	public List<GroupDto> searchGroupDesc(String word) throws Exception;
+	public List<GroupDto> searchCategory(String word) throws Exception;
+	public List<GroupDto> searchRegion(String word) throws Exception;
 }
