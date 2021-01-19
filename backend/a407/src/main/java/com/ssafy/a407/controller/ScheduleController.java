eebec57eacc757600e48ac8a0a560a03586cbbb7
WOOHJ <multicampus@DESKTOP-KVCQHCD>
2021-01-19 18:15:43 +0900

    #HJ06 - 그룹 스케줄 api, 개인 스케줄 보기 api, 스테줄 수정 api, 스케줄 삭제 api 구현

--- backend/a407/src/main/java/com/ssafy/a407/controller/ScheduleController.java
+++ backend/a407/src/main/java/com/ssafy/a407/controller/ScheduleController.java
@@ -1,6 +1,7 @@
 package com.ssafy.a407.controller;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import org.slf4j.Logger;
@@ -8,9 +9,12 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -81,4 +85,101 @@ public class ScheduleController {
 		return entity;
 	}
 	
+	@GetMapping(value = "/grouplist")
+	private ResponseEntity groupScheduleList(@RequestParam int gId) {
+//		System.out.println("[controller] Schedule detail sId >> "+ sId);
+		ResponseEntity entity = null;
+		Map result = new HashMap();
+		
+		try {
+			List<ScheduleDto> list = service.searchGroupSchedule(gId);
+			if(list != null) {
+//				System.out.println("return schedule >> " + schedule.toString());
+				result.put("list", list);
+				result.put("success", "success");
+				entity = new ResponseEntity<>(result, HttpStatus.OK);	
+			}else {
+	        	result.put("success", "fail");
+	        	entity = new ResponseEntity(result, HttpStatus.OK);
+			}
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			result.put("success", "error"); 
+	        entity = new ResponseEntity<>(result, HttpStatus.BAD_REQUEST);
+		}		
+		//System.out.println(entity.getBody().toString());
+		return entity;
+	}
+	
+	@GetMapping(value = "/perlist")
+	private ResponseEntity personalScheduleList(@RequestParam String email) {
+//		System.out.println("[controller] Schedule detail sId >> "+ sId);
+		ResponseEntity entity = null;
+		Map result = new HashMap();
+		
+		try {
+			List<ScheduleDto> list = service.searchPersonalSchedule(email);
+			if(list != null) {
+//				System.out.println("return schedule >> " + schedule.toString());
+				result.put("list", list);
+				result.put("success", "success");
+				entity = new ResponseEntity<>(result, HttpStatus.OK);	
+			}else {
+	        	result.put("success", "fail");
+	        	entity = new ResponseEntity(result, HttpStatus.OK);
+			}
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			result.put("success", "error"); 
+	        entity = new ResponseEntity<>(result, HttpStatus.BAD_REQUEST);
+		}		
+		//System.out.println(entity.getBody().toString());
+		return entity;
+	}
+	
+	@PutMapping(value = "/update")
+	private ResponseEntity update(@RequestBody ScheduleDto schedule) {
+		ResponseEntity entity = null;
+		Map result = new HashMap();
+		
+		try {
+			if(service.modifySchedule(schedule) == 1) {
+				result.put("success", "success");
+				entity = new ResponseEntity(result, HttpStatus.OK);
+			}else {
+	        	result.put("success", "fail");
+	        	entity = new ResponseEntity(result, HttpStatus.OK);
+			}
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			result.put("success", "error"); 
+	        entity = new ResponseEntity<>(result, HttpStatus.BAD_REQUEST);
+		}		
+		return entity;
+	}
+	
+	@DeleteMapping(value = "/delete")
+	private ResponseEntity delete(@RequestParam int sId) {
+		ResponseEntity entity = null;
+		Map result = new HashMap();
+		
+		try {
+			if(service.removeSchedule(sId) == 1) {
+				result.put("success", "success");
+				entity = new ResponseEntity(result, HttpStatus.OK);
+			}else {
+	        	result.put("success", "fail");
+	        	entity = new ResponseEntity(result, HttpStatus.OK);
+			}
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			result.put("success", "error"); 
+	        entity = new ResponseEntity<>(result, HttpStatus.BAD_REQUEST);
+		}		
+		return entity;
+	}
 }
