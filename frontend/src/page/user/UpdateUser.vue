0060edfde89eff08264394a3e7f338d475c81cb6
seokholee <dldlsh2309@gmail.com>
2021-01-20 18:06:53 +0900

    #SH01 - 회원정보 수정 페이지 진행 중

--- frontend/src/page/user/UpdateUser.vue
+++ frontend/src/page/user/UpdateUser.vue
@@ -9,26 +9,38 @@
         <div id="login_info">
           <span> 아이디(이메일) </span>
           <p>aaaaa12345@naver.com</p>
+          <br>
           <span> 비밀번호 </span>
+          <p id="fakePw"> ········ </p>
           <button
             id="update_btn"
             @click="
-              addPwInput();
               div_hide();
             "
           >
             변경
           </button>
           <div id="addedKeyword"></div>
-          <button @click = "div_show()">변경취소</button>
-
-          <p id="fakePw">.........</p>
+          
+          <div  id = "cancel" style = "display:none" >
+             <input type="password" style="margin-top: 10px;" placeholder="현재 비밀번호를 입력하세요" >
+             <br>
+             <input type="password" style="margin-top: 10px;" placeholder="새 비밀번호를 입력하세요" >
+             <br>
+             <input type="password" style="margin-top: 10px;"  placeholder="새 비밀번호를 한 번 더입력하세요" >
+             <br>
+            <button id = "cancel_btn" @click = "div_show()">변경취소</button>
+            &nbsp;
+            <button id = "confirm_btn" >확인</button>
+          </div>
         </div>
         <br /><br />
         <p style="font-size: large">회원 정보</p>
         <div id="member_info">
           <div class="inpbx" style="font-size: large">
-            닉네임 : <input type="text" id="user-id" placeholder="닉네임" />
+            닉네임  <input type="text" id="user-id" placeholder="닉네임" style = "margin-left : 23px"/><br>
+            휴대폰 번호  <input type="text" id="user-id" placeholder="휴대폰 번호" /><br>
+            사는 곳  <input type="text" id="user-id" placeholder="사는 곳" /><br>
           </div>
         </div>
       </div>
@@ -47,11 +59,9 @@ export default {
       console.log('123');
       var addedFormDiv = document.getElementById('addedKeyword');
       var str =
-        '<input type="text" style="margin-top: 10px;"'
-        +'placeholder="현재 비밀번호를 입력하세요" ><br><input type="text" style="margin-top: 10px;"'
-        + 'placeholder="새 비밀번호를 입력하세요" ><br><input type="text" style="margin-top: 10px;"  placeholder="새 비밀번호를 한 번 더입력하세요" ><br>'
-        + '<button @click = "div_show()">변경취소</button>'
-        + '<button>확인</button>';
+        '<input type="text" style="margin-top: 10px;"' +
+        'placeholder="현재 비밀번호를 입력하세요" ><br><input type="text" style="margin-top: 10px;"' +
+        'placeholder="새 비밀번호를 입력하세요" ><br><input type="text" style="margin-top: 10px;"  placeholder="새 비밀번호를 한 번 더입력하세요" ><br>';
       var addedDiv = document.createElement('div');
       // addedDiv.setAttribute("id","keyword_Frm" + count);
       addedDiv.innerHTML = str;
@@ -60,14 +70,15 @@ export default {
     div_hide() {
       document.getElementById('update_btn').style.display = 'none';
       document.getElementById('fakePw').style.display = 'none';
+       document.getElementById('cancel').style.display = 'block';
     },
     div_show() {
-        console.log("zzz");
-        document.getElementById('update_btn').style.display = 'block';
+      console.log('zzz');
+      document.getElementById('update_btn').style.display = 'block';
       document.getElementById('fakePw').style.display = 'block';
       document.getElementById('addedKeyword').style.display = 'none';
- }
-
+      document.getElementById('cancel').style.display = 'none';
+    },
   },
   components: {
     'button-counter': {
@@ -84,10 +95,16 @@ export default {
   border-top-width: 4px;
   border-top-style: solid;
   border-top-color: rgb(0, 0, 0);
+  width: 800px;
+  margin:0 auto;
 }
 
 #login_info,
 #member_info {
+  margin:0 auto;
+  font-size: 18px;
+  width: 600px;
+  text-align: center;
   padding-top: 18px;
   border-top: 4px solid #000;
   border-top-width: 2px;
@@ -95,15 +112,18 @@ export default {
   border-top-color: rgb(172, 159, 159);
 }
 
-#update_btn {
-  width: 34px;
-  height: 50px;
+#update_btn,
+#cancel_btn,
+#confirm_btn {
+  margin:0 auto;
+  width: 70px;
+  height: 30px;
   background: rgb(245, 245, 245);
   border-radius: 2px;
   color: rgb(32, 23, 23);
   font-size: 16px;
   font-weight: 600;
-  text-align: center;
-  margin: 12px 0 10px;
+
+  
 }
 </style>
