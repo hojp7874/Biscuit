81a3046be013be588b3d4ba19267613d09943606
WOOHJ <WOOHJ@HJ-gram>
2021-01-25 11:37:10 +0900

    #HJ08 - 댓글 조회 API 수정

--- backend/a407/src/main/java/com/ssafy/a407/controller/ReplyController.java
+++ backend/a407/src/main/java/com/ssafy/a407/controller/ReplyController.java
@@ -12,6 +12,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -33,7 +34,7 @@ public class ReplyController {
 	
 	@PostMapping(value = "/create")
     private ResponseEntity register(@RequestBody ReplyDto reply) {
-//		System.out.println("[controller] Schedule create >> "+ schedule.toString());
+//		System.out.println("[controller] registReply >> "+ reply.toString());
 		ResponseEntity entity = null;
 		Map result = new HashMap();
 		
@@ -54,11 +55,11 @@ public class ReplyController {
 		return entity;
 	}
 
-	@PostMapping(value = "/update")
+	@PutMapping(value = "/update")
     private ResponseEntity update(@RequestBody ReplyDto reply) {
+//		System.out.println("수정 " +reply);
 		ResponseEntity entity = null;
 		Map result = new HashMap();
-		
 		try {
 			if(service.modifyReply(reply) == 1) {
 				result.put("success", "success");
@@ -78,6 +79,7 @@ public class ReplyController {
 	
 	@DeleteMapping(value = "/delete")
 	private ResponseEntity delete(@RequestParam int rId) {
+//		System.out.println("delete >> "+rId);
 		ResponseEntity entity = null;
 		Map result = new HashMap();
 		
@@ -101,13 +103,15 @@ public class ReplyController {
 	
 	@GetMapping(value = "/list")
 	private ResponseEntity getReplyList(@RequestParam int page, @RequestParam int bId) {
-//		System.out.println("[controller] Reply page >> "+ page);
+//		System.out.println("[controller] Reply List >> page : "+page+"  / bId  : "+bId);
+		Map tmp = new HashMap();
+
 		ResponseEntity entity = null;
 		Map result = new HashMap();
 		try {
 			//전체 댓글 갯수
 			int listCnt = service.getReplyListCnt(bId);
-			System.out.println("listCnt >> " + listCnt);
+//			System.out.println("listCnt >> " + listCnt);
 			//페이징 객체 생성
 			Pagination pagination = new Pagination(listCnt,page);
 			//pagination.pageInfo(page,20, listCnt);
@@ -115,10 +119,11 @@ public class ReplyController {
 			Map pageMap = new HashMap();
 			pageMap.put("startIndex", pagination.getStartIndex());
 			pageMap.put("pageSize", pagination.getPageSize());
+			pageMap.put("bId",bId);
 			
 			List<ReplyDto> list = service.getReplyList(pageMap);
 			if(list != null) {
-				System.out.println("return reply >> " + list.toString());
+//				System.out.println("return reply >> " + list.toString());
 			
 				result.put("success", "success");
 				result.put("pagination", pagination);
