e77371b4dc408cb950a69f5172d1d4026827ebc1
이석호 <dldlsh2309@gmail.com>
2021-02-02 11:21:20 +0900

    #SH19 - 이메일 인증 백엔드 코드 수정

--- backend/a407/src/main/java/com/ssafy/a407/service/EmailServiceImpl.java
+++ backend/a407/src/main/java/com/ssafy/a407/service/EmailServiceImpl.java
@@ -31,12 +31,12 @@ public class EmailServiceImpl implements EmailService {
 		msgg += "<div style='margin:100px;'>";
 		msgg += "<h1> 안녕하세요  <strong>스터디구해보자</strong>입니다!!! </h1>";
 		msgg += "<br>";
-		msgg += "<p>아래 코드를 회원가입 창으로 돌아가 입력해주세요<p>";
+		msgg += "<p>아래 코드를 창으로 돌아가 입력해주세요<p>";
 		msgg += "<br>";
 		msgg += "<p>감사합니다!<p>";
 		msgg += "<br>";
 		msgg += "<div align='center' style='border:1px solid black; font-family:verdana';>";
-		msgg += "<h3 style='color:blue;'>회원가입 코드입니다.</h3>";
+		msgg += "<h3 style='color:blue;'>이메일인증 코드입니다.</h3>";
 		msgg += "<div style='font-size:130%'>";
 		msgg += "CODE : <strong>";
 		msgg += ePw + "</strong><div><br/> ";
