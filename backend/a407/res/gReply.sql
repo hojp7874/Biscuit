9eb7aa671a0b8513a212436ca3545e50dfaf73a7
HJ W <woohjm@naver.com>
2021-02-08 14:57:31 +0900

    #HJ20 - 스터디 그룹용 게시판 댓글 sql 스크립트 추가, Dto 추가

---
+++ backend/a407/res/gReply.sql
@@ -0,0 +1,31 @@
+CREATE TABLE `a407`.`gReply` (
+  `rId` INT NOT NULL AUTO_INCREMENT,
+  `bId` INT NOT NULL,
+  `email` VARCHAR(30) NOT NULL,
+  `nickname` VARCHAR(20) NOT NULL,
+  `contents` VARCHAR(300) NOT NULL,
+  `date` DATETIME NOT NULL,
+  PRIMARY KEY (`rId`),
+  UNIQUE INDEX `rId_UNIQUE` (`rId` ASC) VISIBLE,
+  INDEX `fk_bId_idx` (`bId` ASC) VISIBLE,
+  INDEX `fk_email_idx` (`email` ASC) VISIBLE,
+  INDEX `fk_nickname_idx` (`nickname` ASC) VISIBLE,
+  CONSTRAINT `fk_gReply_bId`
+    FOREIGN KEY (`bId`)
+    REFERENCES `a407`.`gBoard` (`bId`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_gReply_email`
+    FOREIGN KEY (`email`)
+    REFERENCES `a407`.`user` (`email`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_gReply_nickname`
+    FOREIGN KEY (`nickname`)
+    REFERENCES `a407`.`user` (`nickname`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_0900_ai_ci
+COMMENT = 'reply for Group Board';
